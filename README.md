024062a3743b61f3f08b8019d3f95a0cede33375
Liam Gibbons <liamwgibbons@gmail.com>
2023-08-24 22:10:44 -0400

    finished testing and added static

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 The site!!!!
 
 Link to tutorial website
-https://docs.djangoproject.com/en/4.2/intro/tutorial05/
+https://docs.djangoproject.com/en/4.2/intro/tutorial07/
 
-We are at improving our view
+we are at adding related objects
+
+the git thing is git push -u originactual master
--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,4 +1,5 @@
 from typing import Any
+from django.db import models
 from django.db.models.query import QuerySet
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
@@ -6,17 +7,22 @@ from django.shortcuts import render, get_object_or_404
 from .models import Question, Choice
 from django.http import Http404
 from django.views import generic
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+    
 
 class ResultsView(generic.DetailView):
     model = Question
